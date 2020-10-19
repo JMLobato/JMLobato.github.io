7fb1706c22d520868d8e3060bfca4e0a4a988018
decanosuitai <decanosuitai@DebLaptop>
2020-10-19 16:47:38 +0200

    estilo e imagenes añadidas

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,16 +19,24 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
+LINKS = (('GitHub', 'https://github.com/JMLobato/JMLobato.github.io'),
+		 ('Pelican', 'https://getpelican.com/'),
          ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),)
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('GitHub', 'https://github.com/JMLobato/JMLobato.github.io'),)
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Specify theme
+THEME = "/home/decanosuitai/pelican/tema/pelican-bootstrap3-lovers"
+BOOTSTRAP_THEME = "lovers"
+STATIC_PATHS = ['images']
+HEADER_IMAGE = "cabecera.jpg"
+PROFILE_PICTURE = "biohazard.png"
